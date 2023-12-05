a80b8be74df50dc56532d94ff510b447c9a05e65
plaanty <145934497+plaanty@users.noreply.github.com>
2023-12-05 11:39:21 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const factEl = document.getElementById("cat-fact")
+//select the input element with the id "cat-fact"
+const catFact = document.getElementById("cat-fact")
 
 async function setup() {
     const response = await fetch("https://cat-fact.herokuapp.com/facts")
     const data = await response.json()
     
-    const facts = Object.keys(data)
-    
-    console.log()
+    const fact = Object.keys(data)
+
+     // Display the fact in the <p> element
+     catFact.textContent = fact.text;
 }
 
 async function loadFact() {
     const response = await fetch('https://cat-fact.herokuapp.com/facts/random')
     const data = await response.json()
     
-    console.log(data.text)
-    factEl.src = data.text;
+    const text = data.text
+
+    console.log(text)
+
+    // Display the fact in the <p> element
+    catFact.textContent = text;
+
+    //catFact.src = data.text;
 }
 
 const buttonEl = document.getElementById("load")
